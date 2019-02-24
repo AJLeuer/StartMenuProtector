e3da09084251cadc72c54b9484a06fb33bd87f48
Adam J. Leuer <aleuer2@illinois.edu>
2019-02-24 05:44:15 -0600

    Moved icon display logic entirely into StartMenuItem

--- Start Menu Protector/View/StartMenuItem.cs
+++ Start Menu Protector/View/StartMenuItem.cs
@@ -9,24 +9,31 @@ namespace StartMenuProtector.View
 {
     public class StartMenuItem : DockPanel
     {
-        public static readonly DependencyProperty IconSourceProperty = DependencyProperty.Register(nameof (IconSource), typeof (ImageSource), typeof (StartMenuItem), new FrameworkPropertyMetadata(propertyChangedCallback: StartMenuItem.UpdateIconSource) { BindsTwoWayByDefault = false });
-        public static readonly DependencyProperty FileProperty = DependencyProperty.Register(nameof (File), typeof (EnhancedFileInfo), typeof (StartMenuItem), new FrameworkPropertyMetadata(propertyChangedCallback: StartMenuItem.UpdateFile) { BindsTwoWayByDefault = false });
+        public static readonly DependencyProperty FileProperty = DependencyProperty.Register(nameof (File), typeof (EnhancedFileSystemInfo), typeof (StartMenuItem), new FrameworkPropertyMetadata(propertyChangedCallback: StartMenuItem.UpdateFile) { BindsTwoWayByDefault = false });
 
         public static Brush TextColor { get; set; } = new SolidColorBrush(Config.TextColor);
         public static Brush DefaultBackgroundColor { get; set; } = new SolidColorBrush(Config.BackgroundColor);
         public static Brush SelectionTextColor { get; set; } = new SolidColorBrush(Config.SelectionTextColor);
         public Brush SelectionBackgroundColor { get; set; }
-        
-        public EnhancedFileInfo File { get; set; }
-        public ImageSource IconSource { get; set; }
+
+        private EnhancedFileSystemInfo file;
+        public EnhancedFileSystemInfo File
+        {
+            get { return file; }
+            set
+            {
+                file = value;
+                updateState();
+            }
+        }
         
         public TextBlock TextBlock { get; set; }
         public Image Image { get; set; }
-        
+
         public StartMenuItem()
         {
             this.Background = DefaultBackgroundColor;
-            Image = new Image { Source = this.IconSource, Margin = new Thickness(left: 5, top: 5, right: 2.5, bottom: 5)};
+            Image = new Image { Margin = new Thickness(left: 5, top: 5, right: 2.5, bottom: 5)};
             TextBlock = new TextBlock { FontSize = Config.FontSize, Foreground = TextColor, Margin = new Thickness(left: 2.5, top: 5, right: 5, bottom: 5), VerticalAlignment = VerticalAlignment.Center};
             this.Children.Add(Image);
             this.Children.Add(TextBlock);
@@ -43,20 +50,21 @@ namespace StartMenuProtector.View
             this.Background = DefaultBackgroundColor;
             TextBlock.Foreground = TextColor;
         }
-        
-        private static void UpdateIconSource(DependencyObject startMenuDataItem, DependencyPropertyChangedEventArgs updatedValue)
+
+        private void updateState()
         {
-            var self = startMenuDataItem as StartMenuItem;
-            self.IconSource = (ImageSource) updatedValue.NewValue;
-            self.Image.Source = self.IconSource;
+            if (File is EnhancedFileInfo)
+            {
+                Image.Source = ((EnhancedFileInfo) File).Icon;
+            }
+            TextBlock.Text = File.Name;
+            TextBlock.ToolTip = File.FullName;
         }
         
         private static void UpdateFile(DependencyObject startMenuDataItem, DependencyPropertyChangedEventArgs updatedValue)
         {
             var self = startMenuDataItem as StartMenuItem;
-            self.File = (EnhancedFileInfo) updatedValue.NewValue;
-            self.TextBlock.Text = self.File.Name;
-            self.TextBlock.ToolTip = self.File.FullName;
+            self.File = (EnhancedFileSystemInfo) updatedValue.NewValue;
         }
 
     }
